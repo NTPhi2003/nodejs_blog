663bfb0172a09f072ffbca578c901c59ce67bc00
Nguyen Thanh Phi <thanhphicv@gmail.com>
2025-01-31 21:57:27 +0700

    add MVC model

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const { create } = require('express-handlebars')
+const exp = require('constants')
+
+const app = express()
+const port = 3000
+
+const route = require('./routes')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
+// HTTP logger
+// app.use(morgan('combined'))
+
+// Template engine
+const hbs = create({
+  extname: '.hbs'
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources\\views'))
+
+// Route init
+route(app)
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
